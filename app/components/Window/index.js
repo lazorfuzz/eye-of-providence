5a9a91f25a5ef833777ecabdc5f831966ce23162
lazorfuzz <leontosy@gmail.com>
2018-03-25 15:23:12 -0700

    Make chunk caching optional

--- app/components/Window/index.js
+++ app/components/Window/index.js
@@ -3,6 +3,17 @@ import styled from 'styled-components';
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
 
+const Cam = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+`;
+
+const Link = styled.a`
+  color: #00E676;
+  text-decoration: none;
+`;
+
 const Wrapper = styled.div`
   width: 250px;
   height: 250px;
@@ -15,26 +26,17 @@ const Wrapper = styled.div`
   border: 1px solid #00E676;
 `;
 
-const Cam = styled.img`
-  width: 100%;
-  height: 100%;
-  border-radius: 10px;
-`;
-
-const Link = styled.a`
-  color: #00E676;
-  text-decoration: none;
-`;
-
 class Window extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <Wrapper>
         <div style={{ height: '85%', marginBottom: '5px' }}>
-          <Cam
-            alt="cam"
-            src={this.props.videoSrc}
-          />
+          <Link href={this.props.link} target="_blank">
+            <Cam
+              alt="cam"
+              src={this.props.videoSrc}
+            />
+          </Link>
         </div>
         <Link href={this.props.link} target="_blank">{this.props.ip}</Link>
       </Wrapper>
--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -59,7 +59,7 @@ module.exports = require('./webpack.base.babel')({
         // All chunks marked as `additional`, loaded after main section
         // and do not prevent SW to install. Change to `optional` if
         // do not want them to be preloaded at all (cached only when first loaded)
-        additional: ['*.chunk.js'],
+        optional: ['*.chunk.js'],
       },
 
       // Removes warning for about `additional` section usage

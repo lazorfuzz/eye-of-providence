dbfe9d192b528108f0d1109a3c56a956ccbe6faa
lazorfuzz <leontosy@gmail.com>
2018-03-25 22:29:10 -0700

    API fix

--- server/api.js
+++ server/api.js
@@ -28,9 +28,24 @@ router.route('/cams')
     db.find({ type: 'Cam' }, (err, docs) => res.json(docs));
   })
   .post((req, res) => {
+    if (!req.body.url) {
+      res.json({ status: false });
+      return;
+    }
     db.update({ type: 'Cam', url: req.body.url }, { type: 'Cam', url: req.body.url }, { upsert: true }, (err, numAffected, affDocs) => {
       res.json(affDocs);
     });
+  });
+
+router.route('/cams/:_id')
+  .get((req, res) => {
+    db.find({ type: 'Cam', _id: req.params._id }, (err, docs) => { // eslint-disable-line
+      res.json(docs);
+    });
+  })
+  .delete((req, res) => {
+    db.remove({ type: 'Cam', _id: req.params._id }); // eslint-disable-line
+    res.json({ status: true });
   });
 
 module.exports = router;
